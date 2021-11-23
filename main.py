ccda2d5006c07d3bdc414d5b6cf99d353bd0c231
Lozikevich <lozikevich@gmail.com>
2021-11-23 22:49:04 +0700

    added actions

--- main.py
+++ main.py
@@ -1,29 +1,33 @@
 from dataclasses import dataclass
 from datetime import datetime
-from typing import List
+from typing import Dict, Iterable
 
 
 @dataclass
 class Message:
-    date_time: str
+    id: int
     author: str
-    text: str
+    message: str
 
 
-messages_in_chat = []
+messages_in_chat: Dict[id, Message] = {}
 
 
-def get_all_messages() -> list[Message]:
-    return messages_in_chat
+def get_all_messages() -> Iterable:
+    return messages_in_chat.values()
 
 
-def add_new_message(text: Message):
-    messages_in_chat.append(text)
+def add_new_message(message: Message):
+    messages_in_chat[message.id] = message
 
 
-def delete_message(index: int):
-    messages_in_chat.pop(index)
+def delete_message(id_message: int):
+    del messages_in_chat[id_message]
 
 
 if __name__ == '__main__':
-    print('hello')
+    add_new_message(Message(1, 'user_1', 'note_1'))
+    add_new_message(Message(2, 'user_2', 'note_2'))
+    print(messages_in_chat)
+    delete_message(1)
+    print(messages_in_chat)
