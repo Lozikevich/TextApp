fd49a776d129a5586b60fa2b18d325e3ecba10df
Lozikevich <lozikevich@gmail.com>
2021-11-27 14:53:33 +0700

    Composition

--- message.py
+++ message.py
@@ -1,54 +1,45 @@
-messages_in_chat = {}
+from typing import Iterable
+from dataclasses import dataclass
 
 
+@dataclass()
 class Message:
-    def __init__(self, time: str, login: str, message: str):
-        self.__time = time
-        self.__login = login
-        self.__message = message
+    time: str
+    login: str
+    message: str
+
+
+class MessageStorage:
+    def __init__(self, folder: str):
+        self.__messages = {}
+        self.__folder = folder
+
+    def get_all(self) -> Iterable[Message]:
+        return self.__messages.values()
+
+    def put_one(self, message: Message):
+        self.__messages[message.time] = message
+
+    def delete_one(self, time: str):
+        del self.__messages[time]
+
+
+class MessageManager:
+    def __init__(self, name: str, storage: MessageStorage):
+        self.__name = name
+        self.__storage = storage
 
     @property
-    def time(self) -> str:
-        return self.__time
+    def get_all_messages(self):
+        return self.__storage.get_all()
 
-    @property
-    def login(self) -> str:
-        return self.__login
+    def add_new_message(self, message):
+        self.__storage.put_one(message)
 
-    @property
-    def message(self) -> str:
-        return self.__message
-
-    @time.setter
-    def time(self, time: str):
-        self.__time = time
-
-    @login.setter
-    def login(self, login: str):
-        self.__login = login
-
-    @message.setter
-    def message(self, message: str):
-        self.__message = message
-
-    @staticmethod
-    def get_all_messages():
-        return messages_in_chat.values()
-
-    @staticmethod
-    def delete_message(message):
-        del messages_in_chat[message]
+    def delete_message(self, time):
+        self.__storage.delete_one(time)
 
 
-    @staticmethod
-    def add_new_message(message):
-        messages_in_chat[message.time] = message
 
-    @staticmethod
-    def get_message(message):
-        return message
-
-    def __repr__(self) -> str:
-        return f'Message(time={self.time}, login={self.login}, message={self.message})'
 
 
